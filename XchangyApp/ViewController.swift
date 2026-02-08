f75f8343613160a8ed9d488d6a2cdd4a4dc6c72a
LeonidPirogov <lv.pirogof@gmail.com>
2026-02-08 14:02:52 +0200

    Initial Commit

---
+++ XchangyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XchangyApp
+//
+//  Created by Leonid on 08.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
