8682a91c0a81f7f102305f4eceb5639e826e2b55
LeonidPirogov <lv.pirogof@gmail.com>
2026-02-17 09:02:47 +0200

    create ExchangeRate model

---
+++ XchangyApp/Scenes/Exchange/ExchangeRate.swift
@@ -0,0 +1,14 @@
+//
+//  ExchangeRate.swift
+//  XchangyApp
+//
+//  Created by Leonid on 17.02.2026.
+//
+
+import Foundation
+
+struct ExchangeRate {
+    let from: Currency
+    let to: Currency
+    let rate: Decimal
+}
