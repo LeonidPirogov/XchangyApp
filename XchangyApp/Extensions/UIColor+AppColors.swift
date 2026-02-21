065f77f55253bb1cf639f7d31df27a1def8482d5
LeonidPirogov <lv.pirogof@gmail.com>
2026-02-21 12:07:18 +0200

    Extract accentGreen color into UIColor extension

---
+++ XchangyApp/Extensions/UIColor+AppColors.swift
@@ -0,0 +1,17 @@
+//
+//  UIColor+AppColors.swift
+//  XchangyApp
+//
+//  Created by Leonid on 21.02.2026.
+//
+
+import UIKit
+
+extension UIColor {
+    static let accentGreen = UIColor(
+        red: 34.0 / 255.0,
+        green: 208.0 / 255.0,
+        blue: 129.0 / 255.0,
+        alpha: 1.0
+    )
+}
---
+++ XchangyApp/Extensions/UIView+AddSubviews.swift
@@ -0,0 +1,8 @@
+//
+//  UIView+AddSubviews.swift
+//  XchangyApp
+//
+//  Created by Leonid on 21.02.2026.
+//
+
+import Foundation
