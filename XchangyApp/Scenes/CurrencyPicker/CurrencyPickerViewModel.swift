c1bb674de32f0b0797ff9e4c5f61a36a52037ca0
LeonidPirogov <lv.pirogof@gmail.com>
2026-03-04 08:54:52 +0200

    Create CurrencyPickerViewModel

--- XchangyApp/Scenes/CurrencyPicker/CurrencyPickerViewModel.swift
+++ XchangyApp/Scenes/CurrencyPicker/CurrencyPickerViewModel.swift
@@ -5,4 +5,23 @@
 //  Created by Leonid on 04.03.2026.
 //
 
-import Foundation
+import UIKit
+
+struct CurrencyPickerViewModel {
+    let title: String
+    let currencies: [Currency]
+    let currencyUIByCode: [String: CurrencyUIConfig]
+    var selected: Currency
+
+    func makeCellModel(at index: Int) -> CurrencyCellModel {
+        let currency = currencies[index]
+        let config = currencyUIByCode[currency.code]
+
+        return CurrencyCellModel(
+            code: currency.code,
+            flag: config.flatMap { UIImage(named: $0.flagImageName) },
+            flagContentsRect: config?.contentRect,
+            isSelected: currency == selected
+        )
+    }
+}
